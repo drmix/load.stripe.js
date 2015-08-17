5eb64cc248274ceb6ba61be79aa195a30ec95372
drmix <drmix90@gmail.com>
2015-08-17 19:14:16 +0300

    new version

--- load.stripe.js
+++ load.stripe.js
@@ -2,227 +2,284 @@
  * load.stripe.js - ajax animation tool
  * https://github.com/drmix/load.stripe.js
  *
- * Copyright 2013 Danil Valgushev
+ * Copyright 2015 Danil Valgushev
  * Released under the MIT license
- *
- * Date: 2013-12-23
  */
 
 
 
-var StripeLoader = function(ctx, options) {
-    var self = this;
+(function(window, $) {
 
-    var _default_options = {
-        gauss_weight: 0.6,
-        gauss_width: 0.05,
-        gauss_stop: 2/3,
-        gauss_move_time: 500,
-        gauss_vanish_time: 200,
+    var optionsDefault = {
+        gaussWeight: 0.6,
+        gaussWidth: 0.05,
+        gaussStop: 2/3,
+        gaussTimeMove: 500,
+        gaussTimeVanish: 200,
 
-        rect_random: 1,
-        rect_width: 15,
-        rect_interval: 800,
-        rect_period: 700,
-        rect_alpha: 1,
-        rect_slowdown_weight: 5,
+        rectRandom: 1,
+        rectWidth: 80,
+        rectInterval: 800,
+        rectTimeMove: 1500,
+        rectSlowdown: 5,
 
-        color: [18, 61, 132]
+        backgroundColor: "#1751af",
+        rectColor: "#FFFFFF"
     };
 
 
-    options = jQuery.extend(_default_options, options);
+    var S_OFF = 0,
+        S_ACTIVE = 1,
+        S_VANISH = 2;
 
 
-    var gauss_weight = options['gauss_weight'];
-    var gauss_width = options['gauss_width'];
-    var gauss_stop = options['gauss_stop'];
-    var gauss_move_time = options['gauss_move_time'];
-    var gauss_vanish_time = options['gauss_vanish_time'];
+    var requestFrame = window.requestAnimationFrame ||
+                       window.webkitRequestAnimationFrame ||
+                       window.mozRequestAnimationFrame ||
+                       window.oRequestAnimationFrame ||
+                       window.msRequestAnimationFrame ||
+                       function(callback) {
+                           setTimeout(function() { callback(new Date().getTime()); }, 1000/60 );
+                       };
 
-    var rect_random = options['rect_random'];
-    var rect_width = options['rect_width'];
-    var rect_interval = options['rect_interval'];
-    var rect_period = options['rect_period'];
-    var rect_alpha = options['rect_alpha'];
-    var rect_slowdown_weight = options['rect_slowdown_weight'];
+    var Animator = function(ctx, options) {
+        this.options = jQuery.extend({}, optionsDefault, options || {});
 
-    var color = options['color'];
+        this.ctx = ctx;
+        this.height = ctx.canvas.height;
+        this.width = ctx.canvas.width;
+        console.log(ctx.canvas);
 
+        this.reset();
+    };
 
+    Animator.prototype.reset = function() {
+        var o = this.options;
 
-    var _gauss_vanish_pos = 0;
-    var _gauss_vanish_begin_time;
+        this.state = S_OFF;
+        this.timeStart = null;
+        this.rectSeed = o.rectRandom ? Math.round(o.rectInterval * Math.random()) : 0;
 
-    /*
-     * Generate moving gaussian stripe
-     */
-    var getStripe = function(width, time, vanish) {
-        var stripe = [];
-        var stop_point = vanish
-                         ? _gauss_vanish_pos + ((1 + gauss_width) * width - _gauss_vanish_pos) * (time - _gauss_vanish_begin_time) / gauss_vanish_time
-                         : Math.round((1 - Math.exp(-time / gauss_move_time)) * gauss_stop * width);
+        this.dtLast = 0;
+        this.peakLast = 0;
 
-        if (!vanish) {
-            _gauss_vanish_begin_time = time;
-            _gauss_vanish_pos = stop_point;
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    };
+
+    Animator.prototype.calcStripe = function(dt) {
+        var o = this.options;
+
+        var stripe = new Array(this.width);
+
+        var peak = (this.state == S_ACTIVE)
+                   ? (1 - Math.exp(-Math.abs(dt) / o.gaussTimeMove)) * o.gaussStop * this.width
+                   : this.peakLast + ((1 + o.gaussWidth) * this.width - this.peakLast) * (dt - this.dtLast) / o.gaussTimeVanish;
+
+        if (this.state == S_ACTIVE) {
+            this.peakLast = peak;
         }
 
-        for (var i = 0; i < width; i++) {
+        for (var i = 0; i < this.width; i++) {
             var x = i + 1;
-            var dx = x - stop_point;
+            var dx = x - peak;
 
-            var gauss_w = dx > 0 ? 1 : gauss_weight;
-            var gauss_w = gauss_weight;
-            var value = (1 - gauss_w) + gauss_w * Math.exp(-(dx * dx) / (2 * width * width * gauss_width * gauss_width))
-            stripe.push(value);
+            var value = (1 - o.gaussWeight) + o.gaussWeight * Math.exp(-(dx * dx) / (2 * this.width * this.width * o.gaussWidth * o.gaussWidth));
+            stripe[i] = value;
         }
         return stripe;
-    }
+    };
 
+    Animator.prototype.drawBackground = function(stripe) {
+        var o = this.options,
+            ctx = this.ctx,
+            width = this.width,
+            height = this.height,
+            len = stripe.length;
 
+        ctx.clearRect(0, 0, width, height);
 
+        ctx.fillStyle = o.backgroundColor;
+        ctx.fillRect(0, 0, width, height);
 
-    var _rect_rnd_start = 0
+        var data = ctx.getImageData(0, 0, width, height);
+        for (var y = 0; y < height; y++) {
+            for (var x = 0; x < len; x++) {
+                var alpha = Math.floor(stripe[x] * 255);
+                data.data[y * 4 * width + x * 4 + 3] = alpha;
+            }
+        }
+        ctx.putImageData(data, 0, 0);
+    };
 
-    /*
-     * Draw awesome moving rectangles
-     */
-    var placeRectsOnStripe = function(ctx, stripe, time) {
-        var width = stripe.length;
-        var rect_speed = width / rect_period;
-        var rect_poses = [];
+    Animator.prototype.drawRectangles = function(dt, stripe) {
+        var o = this.options,
+            ctx = this.ctx,
+            width = stripe.length,
+            height = ctx.canvas.height,
+            rects = [];
 
-        time = time + _rect_rnd_start;
-
-        for (var i = (time % rect_interval) * rect_speed - (rect_width/2); i < (1 + rect_slowdown_weight) * (width + rect_width); i += rect_interval * rect_speed) {
-            rect_poses.push(Math.round(i));
+        // rectangles move non-linearly
+        var transform_map = [];
+        var transform_sum = 0;
+        for (var i = 0; i < width; i++) {
+            transform_map[i] = i + o.rectSlowdown * transform_sum;
+            transform_sum += stripe[i]
         }
 
-        if (rect_poses.length < 1) {
+        var widthTransformed = width + transform_sum * o.rectSlowdown,
+            speed = widthTransformed / o.rectTimeMove;
+
+        for (var i = ((dt + this.rectSeed) % o.rectInterval) * speed - (o.rectWidth / 2); i < (widthTransformed + o.rectWidth); i += o.rectInterval * speed) {
+            rects.push(Math.round(i));
+        }
+
+        if (rects.length < 1) {
             return;
         }
 
-        // for more awesomeness rects move not linearly
-        var _transform_map = [];
-        var _transform_sum = 0;
-        for (var i = 0; i < width; i++) {
-            _transform_map[i] = i + rect_slowdown_weight * _transform_sum;
-            _transform_sum += stripe[i]
-        }
+        ctx.fillStyle = o.rectColor;
 
-        var width = ctx.canvas.width,
+        var last = 0;
+        for (var i = 0; i + 1 < width && last < rects.length; i++) {
+            var x1 = transform_map[i],
+                x2 = transform_map[i + 1],
+                x = rects[last];
+
+            if (x1 <= x && x < x2) {
+                var xReal = i + ((x - x1) / (x2 - x1));
+                var widthReal = o.rectWidth / (1 + o.rectSlowdown * (x2 - x1));
+                ctx.fillRect(xReal - (widthReal / 2), 0, widthReal, height);
+
+                while (rects[last] < x2 && last < rects.length) {
+                    last++;
+                }
+            }
+        }
+    };
+
+
+    Animator.prototype.draw = function(dt) {
+        var ctx = this.ctx,
             height = ctx.canvas.height,
-            half = Math.floor(rect_width / 2);
-
-        ctx.fillStyle = 'rgba(255, 255, 255, ' + (+rect_alpha) + ')';
-
-        var _last = 0;
-        for (var i = 0; i + 1 < width && _last < rect_poses.length; i++) {
-            var x1 = _transform_map[i],
-                x2 = _transform_map[i + 1],
-                rect_x = rect_poses[_last];
-
-            if (x1 <= rect_x && rect_x < x2) {
-                var rect_transform_x = i + ((rect_x - x1) / (x2 - x1));
-                var rect_transform_width = rect_width / (1 + rect_slowdown_weight * stripe[i]);
-                ctx.fillRect(rect_transform_x - (rect_transform_width / 2), 0, rect_transform_width, height);
-
-                while (rect_poses[_last] < x2 && _last < rect_poses.length) {
-                    _last++;
-                }
-            }
-        }
-    }
-
-
-
-    /*
-     * Draw Stripe on the canvas
-     */
-    var generateImageData = function(ctx, width, height, stripe, color) {
-        var data = ctx.createImageData(width, height);
-        for (var y = 0; y < height; y++) {
-            for (var x = 0; x < width; x++) {
-                for (var c = 0; c < 4; c++) {
-                    var intensity = (c < 3) ? color[c] : Math.floor(stripe[x] * 255);
-                    data.data[y * 4 * width + x * 4 + c] = intensity;
-                }
-            }
-        }
-
-        return data;
-    }
-
-
-
-
-    var _image_data_cache = null, _stripe_cache = null;
-
-    /*
-     * Draw whole picture
-     */
-    var draw = function (ctx, time, vanish) {
-        var height = ctx.canvas.height,
             width = ctx.canvas.width;
 
-        var data, stripe;
-        if (!vanish && time > gauss_move_time * 2 && _image_data_cache && _stripe_cache) {
-            data = _image_data_hash;
-            stripe = _stripe_cache;
-        } else {
-            var stripe = getStripe(width, time, vanish);
-            var data = generateImageData(ctx, width, height, stripe, color);
-            _stripe_cache = stripe;
-            _image_data_hash = data;
+        var stripe = this.calcStripe(dt);
+
+        this.drawBackground(stripe);
+        this.drawRectangles(dt, stripe);
+    };
+
+
+    Animator.prototype.start = function() {
+        if (this.state == S_ACTIVE) {
+            return;
         }
-        ctx.putImageData(data, 0, 0);
+        this.reset();
+        this.state = S_ACTIVE;
 
-        placeRectsOnStripe(ctx, stripe, time);
-    }
-
-
-
+        var o = this.options,
+            self = this;
 
 
-    var _animator = null;
-    var _vanish = false;
-
-    this.Reset = function() {
-        if (_animator) {
-            clearInterval(_animator);
-        }
-        _vanish = false;
-        _gauss_vanish_pos = 0;
-        _gauss_vanish_begin_time = 0;
-        _rect_rnd_start = 0;
-
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    }
-
-    this.Start = function() {
-        self.Reset();
-
-        var start_time = new Date().getTime();
-        _rect_rnd_start = (rect_random) ? Math.round(rect_interval * Math.random()) : 0
-
-        _animator = setInterval(function() {
-            var current_time = new Date().getTime();
-            var time = current_time - start_time;
-
-            draw(ctx, time, _vanish);
-
-            if (_vanish && (time - _gauss_vanish_begin_time) > gauss_vanish_time) {
-                self.Reset();
+        var handler = function(time) {
+            if (typeof time === "undefined" || self._handler !== handler) {
+                return;
             }
-        }, 10);
+
+            if (self.timeStart === null || time < self.timeStart) {
+                self.timeStart = time;
+            }
+            var dt = time - self.timeStart;
+
+            if (self.state == S_OFF || (self.state == S_VANISH && dt > (self.dtLast + o.gaussTimeVanish))) {
+                self.reset();
+                return;
+            }
+            if (self.state == S_ACTIVE) {
+                self.dtLast = dt;
+            }
+
+            self.draw(dt);
+
+            requestFrame(handler);
+        }
+        self._handler = handler;
+
+        requestFrame(handler);
+    };
+
+    Animator.prototype.stop = function() {
+        if (this.state == S_ACTIVE) {
+            this.state = S_VANISH;
+        }
+    };
+
+    Animator.prototype.updateOptions = function(options) {
+        this.options = jQuery.extend({}, optionsDefault, options);
+    };
+
+
+
+    var animations = [];
+
+    var getAnimator = function(element) {
+        var animator = null;
+        for (var i = 0; i < animations.length; i++) {
+            if (animations[i][0] === element) {
+                animator = animations[i][1];
+                break;
+            }
+        }
+        if (animator === null && element && element.getContext) {
+            var ctx = element.getContext('2d');
+            if (ctx) {
+                animator = new Animator(ctx);
+                animations.push([element, animator]);
+            }
+        }
+        return animator;
     }
 
-    this.End = function() {
-        _vanish = true;
+    var removeAnimator = function(element) {
+        var index = -1;
+        for (var i = 0; i < animations.length; i++) {
+            if (animations[i][0] === element) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            animations.splice(index, 1);
+            console.log(animations);
+        }
     }
 
+    if (typeof $ !== "undefined" && $.fn) {
+        $.fn.stripeLoader = function(data) {
+            this.filter("canvas").each(function(i, v) {
+                var animator = getAnimator(v);
+                if (animator === null) {
+                    return;
+                }
 
-    this.Reset();
-};
+                if ("start" == data) {
+                    animator.start();
+                } else if ("reset" == data) {
+                    animator.reset();
+                } else if ("remove" == data) {
+                    animator.reset();
+                    removeAnimator(v);
+                } else if ("stop" == data) {
+                    animator.stop();
+                }
+
+                if (typeof data !== "string") {
+                    animator.updateOptions(data);
+                }
+            });
+            return this;
+        };
+    }
+
+})(window, jQuery);
 
